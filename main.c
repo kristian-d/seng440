2fd9218c31034edeb8b0fb0a4d3f3fb44b223b2d
Kristian Darlington <kristian.f.darlington@gmail.com>
2020-07-18 16:26:48 -0700

    cleaned up the code, added debugging features, and can now output the resulting RGB image

--- main.c
+++ main.c
@@ -6,41 +6,46 @@
 #define STBI_NO_FAILURE_STRINGS
 #define STBI_ASSERT(x)
 #include "stb/stb_image.h"
-
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
 #include "rgb_to_ycc.h"
 
 int main(int argc, char **argv) {
-  if (argc < 2) {
-    printf("Please provide the image location as an argument\n");
+  if (argc < 3) {
+    printf("Please provide the input image location and output image location as arguments\n");
     exit(1);
-  } else if (argc > 2) {
-    printf("Please provide only the image location as an argument\n");
+  } else if (argc > 3) {
+    printf("Please provide only the input and output image locations as arguments\n");
     exit(1);
   }
 
-  char *filename = *(argv + 1);
+  char *infilename = *(argv + 1);
+  char *outfilename = *(argv + 2);
   int width, height, bpp;
 
   // 3 is the number of channels we want, bpp is the actual number of channels contained in the image (bytes per pixel)
-  uint8_t *rgb_img = stbi_load(filename, &width, &height, &bpp, 3);
+  uint8_t *rgb_img = stbi_load(infilename, &width, &height, &bpp, 3);
   if (rgb_img == NULL) {
     printf("The image failed to load");
     exit(1);
   }
-  printf("Loaded image from %s\n", filename);
+  printf("Input image loaded from '%s'\n", infilename);
   if (width != 1280 || height != 1024) {
     printf("Unexpected image dimensions width=%d, height=%d\nExpected 1280x1024\n", width, height);
     exit(1);
   }
 
   ycc_image_t *ycc_image = rgb_to_ycc(rgb_img, width, height);
-  uint8_t *rgb_after = ycc_to_rgb(ycc_image);
-  rgb_image_compare(rgb_img, rgb_after, width, height, 0);
-  free(rgb_after);
   printf("Completed RGB->YCC conversion\n");
-  stbi_image_free(rgb_img);
-  printf("Freed memory allocated for RGB image\n");
+  uint8_t *rgb_img_out = ycc_to_rgb(ycc_image);
+  printf("Completed YCC->RGB conversion\n");
   ycc_image_free(ycc_image);
   printf("Freed memory allocated for YCC image\n");
+  stbi_image_free(rgb_img);
+  printf("Freed memory allocated for input RGB image\n");
+  stbi_write_jpg(outfilename, width, height, bpp, rgb_img_out, 100);
+  printf("Output RGB image written to '%s'\n", outfilename);
+  free(rgb_img_out);
+  printf("Freed memory allocated for output RGB image\n");
   return 0;
 }
--- rgb_to_ycc.c
+++ rgb_to_ycc.c
@@ -3,70 +3,8 @@
 #include <stdlib.h>
 #include "rgb_to_ycc.h"
 
-void rgb_image_print_hex(uint8_t *img, int width, int height) {
-  int idx = 0;
-  for (int i = 0; i < height; i++) {
-    idx = i*width*3;
-    for (int j = 0; j < width*3; j += 3) {
-      printf("0x%02x%02x%02x ", *(img + idx + j), *(img + idx + j + 1), *(img + idx + j + 2));
-    }
-    printf("\n");
-  }
-}
-
-void rgb_image_print(uint8_t *img, int width, int height) {
-  int idx = 0;
-  for (int i = 0; i < 1; i++) { // TODO change this back to use height, not 1
-    idx = i*width*3;
-    for (int j = 0; j < width*3; j += 3) {
-      printf("(%d,%d,%d) ", *(img + idx + j), *(img + idx + j + 1), *(img + idx + j + 2));
-    }
-    printf("\n");
-  }
-}
-
-void rgb_image_compare(uint8_t *rgb_image_1, uint8_t *rgb_image_2, int width, int height, int print) {
-  int idx = 0, flag = 0, max_error_red = 0, max_error_green = 0, max_error_blue = 0, red1, red2, green1, green2, blue1, blue2;
-  for (int i = 0; i < height; i++) { // TODO change this back to use height, not 1
-    idx = i*width*3;
-    for (int j = 0; j < width*3; j += 3) {
-      red1 = *(rgb_image_1 + idx + j); red2 = *(rgb_image_2 + idx + j); green1 = *(rgb_image_1 + idx + j + 1); green2 = *(rgb_image_2 + idx + j + 1); blue1 = *(rgb_image_1 + idx + j + 2); blue2 = *(rgb_image_2 + idx + j + 2);
-      if (red1 - red2 > max_error_red) {
-        flag = 1;
-        max_error_red = red1 - red2;
-      } else if (red2 - red1 > max_error_red) {
-        flag = 1;
-        max_error_red = red2 - red1;
-      } if (green1 - green2 > max_error_green) {
-        flag = 1;
-        max_error_green = green1 - green2;
-      } else if (green2 - green1 > max_error_green) {
-        flag = 1;
-        max_error_green = green2 - green1;
-      } if (blue1 - blue2 > max_error_blue) {
-        flag = 1;
-        max_error_blue = blue1 - blue2;
-      } else if (blue2 - blue1 > max_error_blue) {
-        flag = 1;
-        max_error_blue = blue2 - blue1;
-      }
-      if (print || flag) printf("(%d->%d,%d->%d,%d->%d) ", red1, red2, green1, green2, blue1, blue2);
-      if (flag && !print) {
-        printf("pixel row=%d column=%d", i, j);
-        printf("\n");
-      }
-      flag = 0;
-    }
-    if (print) printf("\n");
-  }
-  printf("max_error_red=%d max_error_green=%d max_error_blue=%d\n", max_error_red, max_error_green, max_error_blue);
-}
-
-// note that the image is has width * 3 bytes per row for RGB
 ycc_image_t *rgb_to_ycc(uint8_t *img, int width, int height) {
-  // for testing to see if image is being read in properly
-  // rgb_image_print_hex(img, width, height);
-
+  // set up YCC image space and metadata
   uint8_t *y  = (uint8_t *)malloc(sizeof(uint8_t)*height*width);   // Y values are calculated 1:1
   uint8_t *cb = (uint8_t *)malloc(sizeof(uint8_t)*height*width/4); // Cb values are calculated downsampled 1:4
   uint8_t *cr = (uint8_t *)malloc(sizeof(uint8_t)*height*width/4); // Cr values are calculated downsampled 1:4
@@ -111,25 +49,19 @@ ycc_image_t *rgb_to_ycc(uint8_t *img, int width, int height) {
       // we can allow for a 24-bit representation; with 24-bit representation, scale factor is 2^24, leaving 8 bits for each RGB multiplication, none for sign (none needed), and none for addition (buffer for addition not needed, as seen below)
       // range of values the sum in brackets can reach is [0 + 0 + 0 = 0, 1099494720 + 2156207580 + 419262585 + 8388608 = 3683353887], both of which can be represented by 32 bits, therefore no bits are needed as a buffer for addition
       // the sum in brackets is immediately scaled down to an 8-bit value so it can be packaged into a uint8_t
-      y[y_index++] = 16 + (((4311744*r + 8455716*g + 1644167*b) + (1 << 23)) >> 24); // y can be written to consecutively, so ++ is ok
-      //ignore: for debugging y[y_index++] = 16 + (((2155872*r + 4227858*g + 822084*b) + (1 << 22)) >> 23); // y can be written to consecutively, so ++ is ok
-      //ignore: for debugging y[y_index++] = 16 + ((16763*r + 32909*g + 6391*b) >> 16); // y can be written to consecutively, so ++ is ok
+      y[y_index++] = 16 + ((4311744*r + 8455716*g + 1644167*b + (1 << 23)) >> 24); // y can be written to consecutively, so ++ is ok
 
       // original coefficients are -0.148, -0.291, 0.439 which can be represented with [-2^-1 = -0.5, 2^-1 = 0.5]
       // we can allow for a 23-bit representation; with 23-bit representation, scale factor is 2^24, leaving 8 bits for each RGB multiplication, 1 bit for sign, and none for addition (buffer for addition not needed, as seen below)
       // range of values sum in brackets can reach is [-633172140 - 1244953350 = -1878125490, 0 + 0 + 7365198 + 8388608 = 15753806], both of which can be represented by 32 bits, therefore no bits are needed as a buffer for addition
       // the sum in brackets is immediately scaled down to an 8-bit value so it can be packaged into a uint8_t
-      cb[cb_index++] = 128 + (((-2483028*r + -4882170*g + 7365198*b) + (1 << 23)) >> 24); // cb can be written to consecutively, so ++ is ok
-      //ignore: for debugging cb[cb_index++] = 128 + (((-1241514*r + -2441085*g + 3682599*b) + (1 << 22)) >> 23); // cb can be written to consecutively, so ++ is ok
-      //ignore: for debugging cb[cb_index++] = 128 + ((-9676*r + -18996*g + 28672*b) >> 16); // cb can be written to consecutively, so ++ is ok
+      cb[cb_index++] = 128 + ((-2483028*r + -4882170*g + 7365198*b + (1 << 23)) >> 24); // cb can be written to consecutively, so ++ is ok
 
       // original coefficients are 0.439, -0.368, -0.071 which can be represented with [-2^-1 = -0.5, 2^-1 = 0.5]
       // we can allow for a 23-bit representation; with 23-bit representation, scale factor is 2^24, leaving 8 bits for each RGB multiplication, 1 bit for sign, and none for addition (buffer for addition not needed, as seen below)
       // range of values sum in brackets can reach is [0 - 1574373825 - 303751496 = -1878125321, 1878125490 - 0 - 0 + 8388608 = 1886514098], both of which can be represented by 32 bits, therefore no bits are needed as a buffer for addition
       // the sum in brackets is immediately scaled down to an 8-bit value so it can be packaged into a uint8_t
-      cr[cr_index++] = 128 + (((7365198*r + -6174015*g + -1191182*b) + (1 << 23)) >> 24); // cr can be written to consecutively, so ++ is ok
-      //ignore: for debugging cr[cr_index++] = 128 + (((3682599*r + -3087008*g + -595591*b) + (1 << 22)) >> 23); // cr can be written to consecutively, so ++ is ok
-      //ignore: for debugging cr[cr_index++] = 128 + ((28672*r + -24009*g + -4662*b) >> 16); // cr can be written to consecutively, so ++ is ok
+      cr[cr_index++] = 128 + ((7365198*r + -6174015*g + -1191182*b + (1 << 23)) >> 24); // cr can be written to consecutively, so ++ is ok
 
       // calculate Y value for second pixel
       pixel_index += 3;
@@ -138,9 +70,7 @@ ycc_image_t *rgb_to_ycc(uint8_t *img, int width, int height) {
       // we can allow for a 24-bit representation; with 24-bit representation, scale factor is 2^24, leaving 8 bits for each RGB multiplication, none for sign (none needed), and none for addition (buffer for addition not needed, as seen below)
       // range of values the sum in brackets can reach is [0 + 0 + 0 = 0, 1099494720 + 2156207580 + 419262585 + 8388608 = 3683353887], both of which can be represented by 32 bits, therefore no bits are needed as a buffer for addition
       // the sum in brackets is immediately scaled down to an 8-bit value so it can be packaged into a uint8_t
-      y[y_index++] = 16 + (((4311744*img[pixel_index] + 8455716*img[pixel_index + 1] + 1644167*img[pixel_index + 2]) + (1 << 23)) >> 24); // y can be written to consecutively, so ++ is ok
-      //ignore: for debugging y[y_index++] = 16 + (((2155872*img[pixel_index] + 4227858*img[pixel_index + 1] + 822084*img[pixel_index + 2]) + (1 << 22)) >> 23); // y can be written to consecutively, so ++ is ok
-      //ignore: for debugging y[y_index++] = 16 + ((16763*img[pixel_index] + 32909*img[pixel_index + 1] + 6391*img[pixel_index + 2]) >> 16); // y can be written to consecutively, so ++ is ok
+      y[y_index++] = 16 + ((4311744*img[pixel_index] + 8455716*img[pixel_index + 1] + 1644167*img[pixel_index + 2] + (1 << 23)) >> 24); // y can be written to consecutively, so ++ is ok
     }
 
     row_start_index += physical_width;
@@ -153,18 +83,56 @@ ycc_image_t *rgb_to_ycc(uint8_t *img, int width, int height) {
       // we can allow for a 24-bit representation; with 24-bit representation, scale factor is 2^24, leaving 8 bits for each RGB multiplication, none for sign (none needed), and none for addition (buffer for addition not needed, as seen below)
       // range of values the sum in brackets can reach is [0 + 0 + 0 = 0, 1099494720 + 2156207580 + 419262585 + 8388608 = 3683353887], both of which can be represented by 32 bits, therefore no bits are needed as a buffer for addition
       // the sum in brackets is immediately scaled down to an 8-bit value so it can be packaged into a uint8_t
-      y[y_index++] = 16 + (((4311744*img[pixel_index] + 8455716*img[pixel_index + 1] + 1644167*img[pixel_index + 2]) + (1 << 23)) >> 24); // y can be written to consecutively, so ++ is ok    }
-      //ignore: for debugging y[y_index++] = 16 + (((2155872*img[pixel_index] + 4227858*img[pixel_index + 1] + 822084*img[pixel_index + 2]) + (1 << 22)) >> 23); // y can be written to consecutively, so ++ is ok
-      //ignore: for debugging y[y_index++] = 16 + ((16763*img[pixel_index] + 32909*img[pixel_index + 1] + 6391*img[pixel_index + 2]) >> 16); // y can be written to consecutively, so ++ is ok
+      y[y_index++] = 16 + ((4311744*img[pixel_index] + 8455716*img[pixel_index + 1] + 1644167*img[pixel_index + 2] + (1 << 23)) >> 24); // y can be written to consecutively, so ++ is ok    }
     }
   }
 
   return ycc_image;
 }
 
+uint8_t *ycc_to_rgb(ycc_image_t *ycc_image) {
+  uint8_t *rgb_image = (uint8_t *)malloc(ycc_image->height*ycc_image->width*3*sizeof(uint8_t));
+  uint8_t y, cb, cr;
+  int r, g, b, rgb_pixel_index;
+  for (int i = 0; i < ycc_image->height; i++) {
+    for (int j = 0; j < ycc_image->width; j++) {
+      rgb_pixel_index = (i*ycc_image->width + j)*3;
+      y = ycc_image->y[i*ycc_image->width + j];
+      cb = ycc_image->cb[i/2 * ycc_image->width/2 + j/2];
+      cr = ycc_image->cr[i/2 * ycc_image->width/2 + j/2];
+      r = (1220542*(y - 16) + 1673527*(cr - 128) + (1 << 19)) >> 20;
+      g = (1220542*(y - 16) - 852492*(cr - 128) - 409993*(cb - 128) + (1 << 19)) >> 20;
+      b = (610271*(y - 16) + 1058013*(cb - 128) + (1 << 18)) >> 19;
+      // the values must saturate instead of wrapping around
+      rgb_image[rgb_pixel_index] = r > 255 ? 255 : (r < 0 ? 0 : r);
+      rgb_image[rgb_pixel_index + 1] = g > 255 ? 255 : (g < 0 ? 0 : g);
+      rgb_image[rgb_pixel_index + 2] = b > 255 ? 255 : (b < 0 ? 0 : b);
+    }
+  }
+  return rgb_image;
+}
+
+void ycc_image_free(ycc_image_t *ycc_image) {
+  free(ycc_image->y);
+  free(ycc_image->cb);
+  free(ycc_image->cr);
+  free(ycc_image);
+}
+
 void ycc_image_debug(ycc_image_t *ycc_image) {
   printf("Debugging YCC image...\n");
-  printf("YCC image stats: pixel_count=%d, width=%d, height=%d, y_bytes=%d, cb_bytes=%d, cr_bytes=%d, total_bytes=%d\n", ycc_image->height*ycc_image->width, ycc_image->width, ycc_image->height, ycc_image->y_bytes, ycc_image->cb_bytes, ycc_image->cr_bytes, ycc_image->total_bytes);
+  long y_min = 2147483647, y_max = -2147483648, cb_min = 2147483647, cb_max = -2147483648, cr_min = 2147483647, cr_max = -2147483648;
+  for (int i = 0; i < ycc_image->height*ycc_image->width; i++) {
+    if (ycc_image->y[i] < y_min) y_min = ycc_image->y[i];
+    if (ycc_image->y[i] > y_max) y_max = ycc_image->y[i];
+  }
+  for (int i = 0; i < ycc_image->height*ycc_image->width/4; i++) {
+    if (ycc_image->cb[i] < cb_min) cb_min = ycc_image->cb[i];
+    if (ycc_image->cb[i] > cb_max) cb_max = ycc_image->cb[i];
+    if (ycc_image->cr[i] < cr_min) cr_min = ycc_image->cr[i];
+    if (ycc_image->cr[i] > cr_max) cr_max = ycc_image->cr[i];
+  }
+  printf("YCC image stats:\n\tpixel_count=%d\n\twidth=%d\n\theight=%d\n\ty_bytes=%d\n\tcb_bytes=%d\n\tcr_bytes=%d\n\ttotal_bytes=%d\n\ty_max=%ld\n\ty_min=%ld\n\tcb_max=%ld\n\tcb_min=%ld\n\tcr_max=%ld\n\tcr_max=%ld\n", ycc_image->height*ycc_image->width, ycc_image->width, ycc_image->height, ycc_image->y_bytes, ycc_image->cb_bytes, ycc_image->cr_bytes, ycc_image->total_bytes, y_max, y_min, cb_max, cb_min, cr_max, cr_min);
   printf("First row of Y:\n");
   for (int i = 0; i < ycc_image->width; i++) {
     printf("%d ", ycc_image->y[i]);
@@ -180,39 +148,62 @@ void ycc_image_debug(ycc_image_t *ycc_image) {
   printf("\nFinished debug.\n");
 }
 
-uint8_t add8_saturating(uint8_t a, uint8_t b) {
-  uint8_t c = a + b;
-  if (c < a) c = -1;
-  return c;
-}
-
-uint8_t sub8_saturating(uint8_t a, uint8_t b) {
-  uint8_t c = a - b;
-  if (c > a) c = 0;
-  return c;
-}
-
-uint8_t *ycc_to_rgb(ycc_image_t *ycc_image) {
-  uint8_t *rgb_image = (uint8_t *)malloc(ycc_image->height*ycc_image->width*3*sizeof(uint8_t));
-  uint8_t y, cb, cr;
-  int rgb_pixel_index;
-  for (int i = 0; i < ycc_image->height; i++) {
-    for (int j = 0; j < ycc_image->width; j++) {
-      rgb_pixel_index = (i*ycc_image->width + j)*3;
-      y = ycc_image->y[i*ycc_image->width + j];
-      cb = ycc_image->cb[(int)i/2 * ycc_image->width/2 + (int)j/2];
-      cr = ycc_image->cr[(int)i/2 * ycc_image->width/2 + (int)j/2];
-      rgb_image[rgb_pixel_index] = add8_saturating(1.164*(y - 16), 1.596*(cr - 128));
-      rgb_image[rgb_pixel_index + 1] = sub8_saturating(sub8_saturating(1.164*(y - 16), 0.813*(cr - 128)), 0.391*(cb - 128));
-      rgb_image[rgb_pixel_index + 2] = add8_saturating(1.164*(y - 16), 2.018*(cb - 128));
+void rgb_image_print_hex(uint8_t *img, int width, int height) {
+  int idx = 0;
+  for (int i = 0; i < height; i++) {
+    idx = i*width*3;
+    for (int j = 0; j < width*3; j += 3) {
+      printf("0x%02x%02x%02x ", *(img + idx + j), *(img + idx + j + 1), *(img + idx + j + 2));
     }
+    printf("\n");
   }
-  return rgb_image;
 }
 
-void ycc_image_free(ycc_image_t *ycc_image) {
-  free(ycc_image->y);
-  free(ycc_image->cb);
-  free(ycc_image->cr);
-  free(ycc_image);
+void rgb_image_print(uint8_t *img, int width, int height) {
+  int idx = 0;
+  for (int i = 0; i < 1; i++) { // TODO change this back to use height, not 1
+    idx = i*width*3;
+    for (int j = 0; j < width*3; j += 3) {
+      printf("(%d,%d,%d) ", *(img + idx + j), *(img + idx + j + 1), *(img + idx + j + 2));
+    }
+    printf("\n");
+  }
+}
+
+// NOTE: usually you will not want to set print_image_comparison as it is a lot of clutter
+void rgb_image_compare(uint8_t *rgb_image_1, uint8_t *rgb_image_2, int width, int height, int print_image_comparison) {
+  int idx = 0, flag = 0, max_error_red = 0, max_error_green = 0, max_error_blue = 0, red1, red2, green1, green2, blue1, blue2;
+  for (int i = 0; i < height; i++) { // TODO change this back to use height, not 1
+    idx = i*width*3;
+    for (int j = 0; j < width*3; j += 3) {
+      red1 = *(rgb_image_1 + idx + j); red2 = *(rgb_image_2 + idx + j); green1 = *(rgb_image_1 + idx + j + 1); green2 = *(rgb_image_2 + idx + j + 1); blue1 = *(rgb_image_1 + idx + j + 2); blue2 = *(rgb_image_2 + idx + j + 2);
+      if (red1 - red2 > max_error_red) {
+        flag = 1;
+        max_error_red = red1 - red2;
+      } else if (red2 - red1 > max_error_red) {
+        flag = 1;
+        max_error_red = red2 - red1;
+      } if (green1 - green2 > max_error_green) {
+        flag = 1;
+        max_error_green = green1 - green2;
+      } else if (green2 - green1 > max_error_green) {
+        flag = 1;
+        max_error_green = green2 - green1;
+      } if (blue1 - blue2 > max_error_blue) {
+        flag = 1;
+        max_error_blue = blue1 - blue2;
+      } else if (blue2 - blue1 > max_error_blue) {
+        flag = 1;
+        max_error_blue = blue2 - blue1;
+      }
+      if (print_image_comparison || flag) printf("(%d->%d,%d->%d,%d->%d) ", red1, red2, green1, green2, blue1, blue2);
+      if (flag && !print_image_comparison) {
+        printf("pixel row=%d column=%d", i, j);
+        printf("\n");
+      }
+      flag = 0;
+    }
+    if (print_image_comparison) printf("\n");
+  }
+  printf("max_error_red=%d max_error_green=%d max_error_blue=%d\n", max_error_red, max_error_green, max_error_blue);
 }
